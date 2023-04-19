2ca3e1fb31a1d23b3a622410c69be66b4b7780b2
Bugsters104 <r.kristaps@inbox.lv>
2023-04-19 12:21:36 +0300

    Sagatavots viss pirmā skripta rakstīšanai

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+	public int kadruSk = 0;
+
+	void Start () {
+		Debug.Log("Uzsākta programma! Tu esi "
+			+kadruSk+" kadrā");
+	}
+	
+	void Update () {
+		Debug.Log("Kadrs: "+kadruSk);
+		kadruSk++;
+	}
+}
